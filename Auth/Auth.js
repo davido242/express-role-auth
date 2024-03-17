8ba08e8dd4fa5ed3027aad0ac2330b9c48edd163
Monday_David_Sarka <davidsarka242@gmail.com>
2024-03-17 12:16:39 +0100

    Added login logic and schema noted for basic user

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -1,20 +1,54 @@
-const connectDB = require('../db');
+const { connectDB } = require('../db');
 
 exports.register = async (req, res) => {
-  const { name, password } = req.body;
+  const { name, email, password, role } = req.body;
   if (password.length < 6) {
     return res.status(400).json({ message: "Password less than 6 characters" })
   }
-  try {
-    await connectDB.query("INSERT into students (name, password) VALUES (?, ?)", [username, password])
-    .then(user => {
-      res.status(200).json({  message: "User successfully created", user})
-    })
+  try {    
+    const getUserQuery = "SELECT * FROM students WHERE name = ?";
+    const getUserResult = await connectDB.query(getUserQuery, [name]);
+
+    if (getUserResult[0].length > 0){
+        const user = getUserResult[0];
+        res.status(409).json({ message: "User Already Exists", user });
+    } else {
+      await connectDB.query("INSERT into students (name, email, password, role) VALUES (?, ?, ?, ?)", [name, email, password, role]);
+        res.status(201).json({ message: "User created" });
+    }
   } catch (error) {
     console.log("🚀 ~ exports.register= ~ error:", error);
-    res.status(401).json({
+    res.status(500).json({
       message: "User not successful created",
-      error: error.mesage,
+      error: error.message,
+    })
+  }
+}
+
+exports.login = async (req, res, next) => {
+  const { name, password } = req.body  
+  if (!name || !password) {
+    return res.status(400).json({
+      message: "Username or Password not present",
+    })
+  }
+  try {
+    const [user] = await connectDB.query("SELECT * FROM students WHERE name = ?", [name]);
+    if (!user[0]) {
+      res.status(401).json({
+        message: "Login not successful",
+        error: "User not found",
+      })
+    } else {
+      res.status(200).json({
+        message: "Login successful",
+        user,
+      })
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: "An error occurred",
+      error: error.message,
     })
   }
 }
--- server.js
+++ server.js
@@ -3,19 +3,16 @@ const app = express();
 const PORT = 5000;
 const { connectDB } = require("./db");
 
-// app.get('/', async (req, res) => {
-//   const [data] = await connectDB.query("INSERT into students (name, email, password, role) VALUES ('david', 'dave@gmail.co', '12345', 'Admin')");
-//   const [data] = await connectDB.query("SELECT * FROM students");
-//   console.log({data: data});
-//   res.send("Welcome bro");
-// })
+app.use(express.json());
+app.use('/api/auth', require("./Auth/Route"));
 
-
+app.get('/', async (req, res) => {
+  res.send("Welcome bro");
+})
 
 const server = app.listen(PORT, () =>
   console.log(`Server Connected to port ${PORT}`)
 )
-
 process.on("unhandledRejection", err => {
   console.log(`An error occurred: ${err.message}`)
   server.close(() => process.exit(1))
